54f3b906b8e8366eb6a392896e613b6e072c58b4
Doug V <dougmvieira@gmail.com>
2019-02-23 18:51:48 +0000

    Request Quandl API key

--- quandl_fetcher.py
+++ quandl_fetcher.py
@@ -2,9 +2,12 @@ import pandas_datareader.data as web
 from parameters import tickdata_start, dailydata_end, closing_time
 
 
+quandl_key = input("Please insert your Quandl API Key: ")
+
 # Continuous front-month E-mini S&P500
 daily_prices = web.DataReader('CHRIS/CME_ES1', 'quandl',
-                              tickdata_start.date(), dailydata_end.date())
+                              tickdata_start.date(), dailydata_end.date(),
+                              access_key=quandl_key)
 
 daily_prices.index += closing_time
 daily_prices.sort_index(inplace=True)
